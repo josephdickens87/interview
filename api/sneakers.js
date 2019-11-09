f21bc88c2f110e676e370edf784373666022b451
josephdickens87 <josephdickens87@gmail.com>
2019-11-09 18:33:06 -0500

    get one works

--- api/sneakers.js
+++ api/sneakers.js
@@ -2,9 +2,20 @@ const express = require('express')
 const router = express.Router()
 const queries = require('../db/queries')
 
+function isValidId(req, res, next){
+    if(!isNaN(req.params.id)) return next();
+    next(new Error('invalid ID'))
+}
+
 router.get('/', (req, res) => {
     queries.getAll().then(sneakers => {
         res.json(sneakers)
+    })
+})
+
+router.get('/:id', isValidId, (req, res) => {
+    queries.getOne(req.params.id).then(sneakers => {
+        res.json(sneakers);
     })
 })
 
--- db/queries.js
+++ db/queries.js
@@ -3,5 +3,8 @@ const knex = require('./knex');
 module.exports = {
     getAll(){
         return knex('sneakers')
+    },
+    getOne(id){
+        return knex('sneakers').where('id', id).first()
     }
 }
