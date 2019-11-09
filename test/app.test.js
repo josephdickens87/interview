8c19ff2a75ab9e122a91012b9a83d649f63916a5
josephdickens87 <josephdickens87@gmail.com>
2019-11-09 18:23:52 -0500

    get all and test passes

--- test/app.test.js
+++ test/app.test.js
@@ -1,10 +1,27 @@
 const knex = require('../db/knex');
+const request = require('supertest');
+const app = require('../app');
+const expect = require('chai').expect;
+const fixtures = require('./fixtures')
 
 describe('derp', () => {
-    before(() => {
+    before((done) => {
         knex.migrate.latest()
             .then(() => {
                 return knex.seed.run();
+            }).then(() => done());
+    })
+
+    it('lists all sneakers', function(done){
+        request(app)
+            .get('/api/sneakers')
+            .set('Accept', 'application/json')
+            .expect('Content-type', /json/)
+            .expect(200)
+            .then((response) => {
+                expect(response.body).to.be.a('array');
+                expect(response.body).to.deep.equal(fixtures.stickers);
+                done();
             })
     })
 })
---
+++ test/fixtures.js
@@ -0,0 +1,8 @@
+const stickers = [
+    { id: 1, name: 'yeezy', color: 'red', price: '600.00' },
+    { id: 2, name: 'jordan', color: 'white', price: '300.00' }
+  ];
+
+module.exports = {
+    stickers
+}
