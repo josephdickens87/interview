8f972956dc3feb9fbc9407c0734d8d1c7cd11738
josephdickens87 <josephdickens87@gmail.com>
2019-11-09 19:04:03 -0500

    test passes

--- api/sneakers.js
+++ api/sneakers.js
@@ -8,14 +8,18 @@ function isValidId(req, res, next){
 }
 
 router.get('/', (req, res) => {
-    queries.getAll().then(sneakers => {
-        res.json(sneakers)
+    queries.getAll().then(sneaker => {
+        res.json(sneaker)
     })
 })
 
-router.get('/:id', isValidId, (req, res) => {
-    queries.getOne(req.params.id).then(sneakers => {
-        res.json(sneakers);
+router.get('/:id', isValidId, (req, res, next) => {
+    queries.getOne(req.params.id).then(sneaker => {
+        if(sneaker){
+            res.json(sneaker)
+        } else {
+            next()
+        }
     })
 })
 
--- test/app.test.js
+++ test/app.test.js
@@ -12,7 +12,7 @@ describe('derp', () => {
             }).then(() => done());
     })
 
-    it('lists all sneakers', function(done){
+    it('lists all sneakers', (done) => {
         request(app)
             .get('/api/sneakers')
             .set('Accept', 'application/json')
@@ -21,6 +21,18 @@ describe('derp', () => {
             .then((response) => {
                 expect(response.body).to.be.a('array');
                 expect(response.body).to.deep.equal(fixtures.sneakers);
+                done();
+            })
+    })
+    it('lists a record by id', (done) => {
+        request(app)
+            .get('/api/sneakers/2')
+            .set('Accept', 'application/json')
+            .expect('Content-type', /json/)
+            .expect(200)
+            .then((response) => {
+                expect(response.body).to.be.a('object');
+                expect(response.body).to.deep.equal(fixtures.sneakers[1]);
                 done();
             })
     })
