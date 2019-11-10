df84b2fa73a2513a225cf75302c00d421a62617a
josephdickens87 <josephdickens87@gmail.com>
2019-11-09 22:24:23 -0500

    returns average fit

--- api/fits.js
+++ api/fits.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 const queries = require('../db/fit-queries')
 
-// function isValidId(req, res, next){
-//     if(!isNaN(req.params.id)) return next();
-//     next(new Error('invalid ID'))
-// }
+function isValidId(req, res, next){
+    if(!isNaN(req.params.id)) return next();
+    next(new Error('invalid ID'))
+}
 
 // function validSneaker(sneaker){
 //     const hasName = typeof sneaker.name == 'string' && sneaker.name.trim() != '';
@@ -17,6 +17,16 @@ const queries = require('../db/fit-queries')
 router.post('/', (req, res, next) => {
     queries.create(req.body).then(fit => {
         res.json(fit[0])
+    })
+})
+
+router.get('/:id', isValidId, (req, res, next) => {
+    queries.avgFit(req.params.id).then(fit => {
+        if(fit){
+            res.json(fit)
+        } else {
+            next()
+        }
     })
 })
 
--- db/fit-queries.js
+++ db/fit-queries.js
@@ -3,5 +3,8 @@ const knex = require('./knex');
 module.exports = {
     create(fit){
         return knex('fit').insert(fit, '*')
+    },
+    avgFit(id){
+        return knex('fit').avg('fit').where('sneaker_id', id)
     }
 }
